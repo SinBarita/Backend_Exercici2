e766d6ddac4cb746c03515b0e058e351a4e25ba7
Rafa <rafarequeni@gmail.com>
2020-11-24 15:17:16 +0100

    Inicial

---
+++ Exercicis.Core/Queries/ItemQuery.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Exercicis.Contracts.Domain.Items;
+
+namespace Exercicis.Core.Queries
+{
+    public class ItemQuery
+    {
+        public ItemQuery() { }
+
+        public IEnumerable<AItem> GetItems()
+        {
+            return MockDatabase.Items;
+        }
+
+        public AItem GetItemById(int id)
+        {
+            return MockDatabase.Items.SingleOrDefault(i => i.Id == id);
+        }        
+    }
+}
---
+++ Exercicis.Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using Exercicis.Contracts.Domain.Orders;
+using Exercicis.Core.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicis.Services
+{
+    public interface IOrderService
+    {
+        IEnumerable<AComanda> GetComandes(string refNumber, DateTime? quan);
+        AComanda GetComanda(int id);
+    }
+
+    public class OrderService
+    {
+        private readonly OrderQuery _oq;
+
+        public OrderService() { _oq = new OrderQuery(); }
+
+        public IEnumerable<AComanda> GetComandes(string refNumber, DateTime? quan)
+        {
+            // TODO
+            return null;
+        }
+
+        public AComanda GetComanda(int id)
+        {
+            return _oq.getComandaById(id);
+        }
+    }
+}
